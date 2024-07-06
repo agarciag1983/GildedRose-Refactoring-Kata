574f308f05cf8098c066a6953d9b47d191ef8ed1
Alexander Garcia Garcia <agarciag1983@gmail.com>
2024-07-06 10:01:38 +0200

    Refactor Program

---
+++ csharp.xUnit/GildedRose/Data/Moq/ItemCollectionMoq.cs
@@ -0,0 +1,65 @@
+﻿using GildedRoseKata.Entities;
+using System.Collections.Generic;
+
+namespace GildedRoseKata.Data.Moq
+{
+    internal static class ItemCollectionMoq
+    {
+        public static IList<Item> GetCollection()
+        {
+            return new List<Item>
+            {
+                new Item {
+                    Name = "+5 Dexterity Vest",
+                    SellIn = 10,
+                    Quality = 20
+                },
+                new Item {
+                    Name = ItemNames.AgedBrie,
+                    SellIn = 2,
+                    Quality = 0
+                },
+                new Item {
+                    Name = "Elixir of the Mongoose",
+                    SellIn = 5,
+                    Quality = 7
+                },
+                new Item {
+                    Name = ItemNames.Sulfuras,
+                    SellIn = 0,
+                    Quality = 80
+                },
+                new Item {
+                    Name = ItemNames.Sulfuras,
+                    SellIn = -1,
+                    Quality = 80
+                },
+                new Item
+                {
+                    Name = ItemNames.BackstagePasses,
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item
+                {
+                    Name = ItemNames.BackstagePasses,
+                    SellIn = 10,
+                    Quality = 49
+                },
+                new Item
+                {
+                    Name = ItemNames.BackstagePasses,
+                    SellIn = 5,
+                    Quality = 49
+                },
+                // this conjured item does not work properly yet
+                new Item {
+                    Name = ItemNames.Conjured,
+                    SellIn = 3,
+                    Quality = 6
+                }
+
+            };
+        }
+    }
+}
--- csharp.xUnit/GildedRose/Program.cs
+++ csharp.xUnit/GildedRose/Program.cs
@@ -1,59 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using GildedRoseKata.Entities;
+﻿using GildedRoseKata.Data.Moq;
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace GildedRoseKata;
 
 public class Program
 {
+    private static int minDays = 2;
+
+    private static int GetDays(string[] args)
+    {
+        var argsDays = args.FirstOrDefault();
+        return argsDays is not null && int.TryParse(argsDays, out int days)
+            ? days : minDays;
+    }
+
     public static void Main(string[] args)
     {
         Console.WriteLine("OMGHAI!");
 
-        IList<Item> items = new List<Item>
-        {
-            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-            new Item
-            {
-                Name = "Backstage passes to a TAFKAL80ETC concert",
-                SellIn = 15,
-                Quality = 20
-            },
-            new Item
-            {
-                Name = "Backstage passes to a TAFKAL80ETC concert",
-                SellIn = 10,
-                Quality = 49
-            },
-            new Item
-            {
-                Name = "Backstage passes to a TAFKAL80ETC concert",
-                SellIn = 5,
-                Quality = 49
-            },
-            // this conjured item does not work properly yet
-            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-        };
-
+        var items = ItemCollectionMoq.GetCollection();
         var app = new GildedRose(items);
 
-        int days = 2;
-        if (args.Length > 0)
+        int days = GetDays(args);
+        for (var day = 0; day < days; day++)
         {
-            days = int.Parse(args[0]) + 1;
-        }
-
-        for (var i = 0; i < days; i++)
-        {
-            Console.WriteLine("-------- day " + i + " --------");
+            Console.WriteLine("-------- day {0} --------", day);
             Console.WriteLine("name, sellIn, quality");
-            for (var j = 0; j < items.Count; j++)
+            foreach (var item in items)
             {
-                Console.WriteLine(items[j].Name + ", " + items[j].SellIn + ", " + items[j].Quality);
+                Console.WriteLine("{0}, {1}, {2}", item.Name, item.SellIn, item.Quality);
             }
             Console.WriteLine("");
             app.UpdateQuality();
